9d57e991a115736fe0d200a1c0a4dcaf063d81b5
VAIVASWATMANU37 <vaivaswatmanu13@gmail.com>
2025-09-05 22:09:23 +0530

    SRT-10

--- TIP.py
+++ TIP.py
@@ -1,6 +1,8 @@
-def total_calc(tip_perc):
-    tip=int(input("ENTER THE PERCENTAGE YOU WANT TO PAY"))
-    tip_Amt=(tip/100)*100
-total_calc(Amt)
-Amt=int(input("ENTER THE AMOUNT YOU PAID"))
-TOTAL=print("YOUR TOTAL AMOUNT WILL BE", Amt+ total_calc())
+def total_calc(amount):
+    tip = int(input("ENTER THE PERCENTAGE YOU WANT TO PAY: "))
+    tip_amt = (tip / 100) * amount
+    total = amount + tip_amt
+    return total
+Amt = int(input("ENTER THE AMOUNT YOU PAID: "))
+TOTAL = total_calc(Amt)
+print("YOUR TOTAL AMOUNT WILL BE:", TOTAL)
